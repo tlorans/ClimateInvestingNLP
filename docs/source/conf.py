8e6f550a64714355468281deb08c668b4296e4a1
tlorans <tlorans@wilshire.com>
2023-02-13 12:09:17 +0100

    added first elements for docs

---
+++ docs/source/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Climate Investing with NLP'
+copyright = '2023, Thomas Lorans'
+author = 'Thomas Lorans'
+
+# The full version, including alpha/beta/rc tags
+release = '0'
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autosectionlabel',
+    "nbsphinx",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Climate Investing with NLP documentation master file, created by
+   sphinx-quickstart on Mon Feb 13 11:56:23 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Climate Investing with NLP's documentation!
+======================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
